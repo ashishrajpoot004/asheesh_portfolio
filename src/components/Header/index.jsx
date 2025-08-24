27bcb6e77d1847d48707c6d02c7af72b2e065b78
asheesh kumar <118677088+ashishrajpoot004@users.noreply.github.com>
2025-08-24 19:57:46 +0530

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -67,7 +67,7 @@ const Header = () => {
             <Facts/>
             <div className="header__info__bottom">
               {/* <button className="btn">Download CV</button> */}
-              <a href="https://drive.google.com/file/d/15KNBxW_Fc0dbB5sSA5UZQMMm6HrRAlR8/view?usp=drivesdk" download ="asheesh_Resume.pdf"  className='btn'>Download CV</a>
+              <a href="https://drive.google.com/file/d/1gPIkxcO7psZbK61rX39nTa-zSehu3spt/view?usp=sharing" download ="asheesh_Resume.pdf"  className='btn'>Download CV</a>
               <a href="https://ashishrajpoot004@gmail.com" className='btn'>Email Me</a>
             </div>
           </div>
